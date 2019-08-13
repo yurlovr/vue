2a3e8f1806c7ad1ba5d20eb0fbe0dd0658c196e5
yurlovr <yurlovr@gmail.com>
2019-08-13 23:06:20 +0500

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,57 @@
 <template>
-    <header class="app__header">
-        <div>
-        <h1 class="app__header-main">
-            Teleop
-        </h1>
-        <span>Язык русский</span>
-        </div>
-    </header>
+  <header class="app__header">
+    <h1 class="app__header-main">
+      Teleop
+    </h1>
+    <p class="app__header-lang">
+      Язык: <span @click="setLanguage(lang)">{{lang}}</span>
+    </p>
+  </header>
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Header",
+
+  computed: {
+      ...mapGetters({getLang:"getLang"}),
+      lang: {
+          get(){
+              return this.getLang;
+          }
+      }
+  },
+  methods: {
+      ...mapActions({setLang:"setLang"}),
+      setLanguage(lang){this.setLang(lang)}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .app__header {
-        height: 10vh;
-        text-align: center;
-        &-main {
-             font-size: 42px;
-             line-height: 48px;
-             font-style: normal;
-             font-weight: 300;
-             color: #96B3D2;
-            margin: 0;
-         }
+.app__header {
+  position: relative;
+  height: 10vh;
+  text-align: center;
+  &-main {
+    font-size: 42px;
+    line-height: 48px;
+    font-style: normal;
+    font-weight: 300;
+    color: #96b3d2;
+    margin: 0;
+  }
+  &-lang {
+    position: absolute;
+    font-size: 15px;
+    color: #96b3d2;
+    top: 0;
+    right: 30px;
+    & > span {
+      cursor: pointer;
+      color: #48bdff;
     }
-
+  }
+}
 </style>
--- src/store.js
+++ src/store.js
@@ -6,19 +6,30 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     userLogin: "",
-    userPassword: ""
+    userPassword: "",
+    lang: "rus",
   },
   getters: {
     getUserLogin: state => state.userLogin,
-    getUserPassword: state => state.userPassword
+    getUserPassword: state => state.userPassword,
+    getLang: state => state.lang,
   },
   mutations: {
     SET_USER_LOGIN: (state, payload) => (state.userLogin = payload),
-    SET_USER_PASSWORD: (state, payload) => (state.userPassword = payload)
+    SET_USER_PASSWORD: (state, payload) => (state.userPassword = payload),
+    SET_LANG: (state,payload) => state.lang = payload,
   },
   actions: {
     setUserLogin: ({ commit }, payload) => commit("SET_USER_LOGIN", payload),
     setUserPassword: ({ commit }, payload) =>
-      commit("SET_USER_PASSWORD", payload)
+      commit("SET_USER_PASSWORD", payload),
+    setLang: ({commit}, payload) => {
+      if (payload === "rus") {
+        commit("SET_LANG","eng")
+      } else {
+        commit("SET_LANG","rus")
+      }
+    }
+
   }
 });
